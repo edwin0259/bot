8e09a84364ce8fafd68492016c4984f6890df6fe
edwin0258 <edwin0258@gmail.com>
2017-08-25 12:51:41 -0700

    update stats

--- auto.sh
+++ auto.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+#!/bin/sh
+while true;
+do
 cd C:\projects\bot
 git add -A 
 git commit -m "update stats"
 echo "Pushing.."
-git push https://github.com/edwin0259/bot.git
-read
+git push https://github.com/edwin0259/bot.git;
+sleep 2;
+done;
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+while true;
+do
+ls
+echo "lol";
+sleep 2;
+done
